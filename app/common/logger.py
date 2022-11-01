d927dfa5bf1fbfaf2a2c3a38382b09a0fd3fd1f0
itacode <9909272+itacode@users.noreply.github.com>
2022-11-01 20:40:08 +0900

    add logger formatter

--- app/common/logger.py
+++ app/common/logger.py
@@ -1,3 +1,8 @@
 import logging
 
 logger = logging.getLogger("common")
+logger.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+ch.setFormatter(formatter)
+logger.addHandler(ch)
