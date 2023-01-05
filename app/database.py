9b08cca0445735cc3fa26a11615d8f68ad78884e
itacode <9909272+itacode@users.noreply.github.com>
2023-01-05 16:31:43 +0900

    add future to db engine

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 
 from app.config import DBConfig
 
-engine = create_engine(DBConfig.MY_SERVICE_DB_URL)
+engine = create_engine(DBConfig.MY_SERVICE_DB_URL, future=True)
 
 # https://docs.sqlalchemy.org/en/14/orm/session_basics.html#using-a-sessionmaker
 db_session = sessionmaker(bind=engine, expire_on_commit=False)
